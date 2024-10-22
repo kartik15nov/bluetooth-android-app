632253cbb6f58a06c67e31ca8059c96bee0e619f
Kartikeswar Mallik <31155935+kartik15nov@users.noreply.github.com>
2024-10-22 23:17:04 +0530

    cleaning up the code

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,23 +10,18 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 
-internal const val ACTION_PERMISSIONS_GRANTED = "BluetoothPermission.permissions_granted"
-internal const val ACTION_PERMISSIONS_DENIED = "BluetoothPermission.permissions_denied"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var on: Button
     private lateinit var off: Button
-    private lateinit var find: Button
+    private lateinit var findButton: Button
 
     private lateinit var listview: ListView
 
@@ -34,6 +29,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var deviceList: ArrayList<String>
     private lateinit var adapters: ArrayAdapter<String>
 
+    @SuppressLint("NewApi")
     @Override
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,7 +37,7 @@ class MainActivity : AppCompatActivity() {
 
         on = findViewById(R.id.button_turn_on)
         off = findViewById(R.id.button_turn_off)
-        find = findViewById(R.id.button_find)
+        findButton = findViewById(R.id.button_find)
         listview = findViewById(R.id.listview)
         deviceList = ArrayList()
 
@@ -57,20 +53,10 @@ class MainActivity : AppCompatActivity() {
         on.setOnClickListener {
             if (!bluetoothAdapter.isEnabled) {
                 val bluetoothIntent: Intent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-                if (ActivityCompat.checkSelfPermission(
-                        this,
-                        Manifest.permission.BLUETOOTH_CONNECT
-                    ) != PackageManager.PERMISSION_GRANTED
-                ) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    startActivityForResult(bluetoothIntent, 2)
+                if (!hasPermission(Manifest.permission.BLUETOOTH_CONNECT)) {
+                    return@setOnClickListener
                 }
+                startActivityForResult(bluetoothIntent, 2)
             } else {
                 Toast.makeText(this, "Bluetooth is already enabled", Toast.LENGTH_SHORT).show()
             }
@@ -78,77 +64,52 @@ class MainActivity : AppCompatActivity() {
 
         off.setOnClickListener {
             if (bluetoothAdapter.isEnabled) {
-                bluetoothAdapter.disable()
-                Toast.makeText(this, "Bluetooth disabled", Toast.LENGTH_SHORT).show()
+                if (!hasPermission(Manifest.permission.BLUETOOTH_CONNECT)) {
+                    return@setOnClickListener
+                }
+                val intent: Intent = Intent("android.bluetooth.adapter.action.REQUEST_DISABLE")
+                startActivityForResult(intent, 12)
             }
         }
 
-        find.setOnClickListener {
-            if (ContextCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.BLUETOOTH_SCAN
-                ) == PackageManager.PERMISSION_DENIED
-            ) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                    ActivityCompat.requestPermissions(
-                        this@MainActivity,
-                        arrayOf(Manifest.permission.BLUETOOTH_SCAN),
-                        2
-                    )
-                }
+        findButton.setOnClickListener {
+            registerReceiver(
+                deviceScanReceiver,
+                IntentFilter(BluetoothDevice.ACTION_FOUND)
+            )
+            adapters = ArrayAdapter(this, android.R.layout.simple_list_item_1, deviceList)
+            listview.setAdapter(adapters)
+
+            if (!hasPermission(Manifest.permission.BLUETOOTH_SCAN)) {
+                return@setOnClickListener
             }
             bluetoothAdapter?.startDiscovery()
         }
-
-        val intentFilter = IntentFilter(BluetoothDevice.ACTION_FOUND)
-        registerReceiver(receiver, intentFilter)
-        adapters = ArrayAdapter(applicationContext, android.R.layout.simple_list_item_1, deviceList)
-        listview.setAdapter(adapters)
     }
 
-    val receiver = object : BroadcastReceiver() {
+    @SuppressLint("MissingPermission")
+    val deviceScanReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
-            // Handle the ACTION_FOUND event here
-            val action = intent.action
-            if (BluetoothDevice.ACTION_FOUND == action) {
-                val device =
-                    intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
-                val deviceName = if (ActivityCompat.checkSelfPermission(
-                        applicationContext,
-                        Manifest.permission.BLUETOOTH_CONNECT
-                    ) != PackageManager.PERMISSION_GRANTED
-                ) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-
-                } else {
-                    device?.name
-                    deviceList.add(device?.name.toString())
+            val action: String = intent.action.toString()
+            when (action) {
+                BluetoothDevice.ACTION_FOUND -> {
+                    val device =
+                        intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
+                    val deviceName = device?.name
+                    deviceList.add(deviceName.toString())
                     adapters.notifyDataSetChanged()
                 }
-
             }
         }
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    private fun hasPermission(permission: String): Boolean {
+        return applicationContext.checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED
+    }
 
-        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            sendBroadcast(Intent(ACTION_PERMISSIONS_GRANTED))
-            finish()
-        } else {
-            sendBroadcast(Intent(ACTION_PERMISSIONS_DENIED))
-            finish()
-        }
+    override fun onDestroy() {
+        super.onDestroy()
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        unregisterReceiver(deviceScanReceiver)
     }
 }
